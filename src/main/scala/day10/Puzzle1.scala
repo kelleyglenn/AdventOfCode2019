d757f37e2436244f41f40995ee299c5a93cba036
kelley.glenn <kelley.glenn@workday.com>
2019-12-10 20:05:42 -0800

    Day 10 part 2

--- src/main/scala/day10/Puzzle1.scala
+++ src/main/scala/day10/Puzzle1.scala
@@ -2,6 +2,12 @@ package day10
 
 object Puzzle1 {
   val asteroid: Char = '#'
+  val space: Char = '.'
+
+  case class Direction(id: String, order: Short)
+
+  val dirIds = Seq("U", "UR", "R", "DR", "D", "DL", "L", "UL")
+  val dirMap: Map[String, Direction] = dirIds.zipWithIndex.map(d => (d._1, Direction(d._1, d._2.toShort))).toMap
 
   case class Pos(x: Int, y: Int) {
     def slope(o: Pos): Double = {
@@ -16,12 +22,12 @@ object Puzzle1 {
       Math.sqrt(diffX * diffX + diffY * diffY)
     }
 
-    def directionTo(o: Pos): String = {
+    def directionTo(o: Pos): Direction = {
       val diffX = o.x - x
       val diffY = o.y - y
       val xDir: String = if (diffX > 0) "R" else if (diffX < 0) "L" else ""
       val yDir: String = if (diffY > 0) "D" else if (diffY < 0) "U" else ""
-      xDir + yDir
+      dirMap(yDir + xDir)
     }
   }
 
@@ -33,20 +39,48 @@ object Puzzle1 {
     closer.isEmpty
   }
 
-  def numberVisible(map: Seq[Seq[Char]], pos: Pos): Int = {
+  def allVisibleAsteroidsFrom(map: Seq[Seq[Char]], pos: Pos): Seq[Pos] = {
     if (map(pos.y)(pos.x) != asteroid) throw new IllegalArgumentException("" + pos + " is not " + asteroid)
     val allOtherAsteroids = allAsteroids(map).filter(_ != pos)
-    val allVisibleAsteroids = allOtherAsteroids.filter(canPosASeePosB(map, pos, _))
-    allVisibleAsteroids.size
+    allOtherAsteroids.filter(canPosASeePosB(map, pos, _))
   }
 
-  private def allAsteroids(map: Seq[Seq[Char]]) = {
+  def numberVisible(map: Seq[Seq[Char]], pos: Pos): Int = {
+    allVisibleAsteroidsFrom(map, pos).size
+  }
+
+  private def allAsteroids(map: Seq[Seq[Char]]): Seq[Pos] = {
     (for (x <- map.head.indices; y <- map.indices) yield {
       Pos(x, y)
     }).filter(p => map(p.y)(p.x) == asteroid)
   }
 
-  def mostVisible(map: Seq[Seq[Char]]): Int = {
-    allAsteroids(map).map(numberVisible(map, _)).max
+  def mostVisible(map: Seq[Seq[Char]]): (Int, Pos) = {
+    allAsteroids(map).map(p=>(numberVisible(map, p), p)).maxBy(_._1)
+  }
+
+  def visibleSortedClockwise(map: Seq[Seq[Char]], pos: Pos): Seq[Pos] = {
+    def posALessThanPosB(posA: Pos, posB: Pos): Boolean = {
+      val dirToA = pos.directionTo(posA)
+      val dirToB = pos.directionTo(posB)
+      if (dirToA.order == dirToB.order) pos.slope(posA) < pos.slope(posB)
+      else dirToA.order < dirToB.order
+    }
+
+    val visibleAsteroids = allVisibleAsteroidsFrom(map, pos)
+    visibleAsteroids.sortWith(posALessThanPosB)
+  }
+
+  def explode(map: Seq[Seq[Char]], targets: Seq[Pos]): Seq[Seq[Char]] = {
+    val mapArray = map.map(_.toArray).toArray
+    targets.foreach(p => mapArray(p.y)(p.x) = space)
+    mapArray.map(_.toSeq).toSeq
+  }
+
+  def explosionOrderFrom(map: Seq[Seq[Char]], pos: Pos): Seq[Pos] = {
+    if (numberVisible(map, pos) == 0) Seq.empty else {
+      val targets = visibleSortedClockwise(map, pos)
+      targets ++ explosionOrderFrom(explode(map, targets), pos)
+    }
   }
 }
--- src/test/scala/day10/Puzzle1Test.scala
+++ src/test/scala/day10/Puzzle1Test.scala
@@ -25,6 +25,13 @@ class Puzzle1Test extends AnyFlatSpec {
     var row3: Seq[Char] = "....#".toSeq
     var row4: Seq[Char] = "...##".toSeq
     var m = Seq(row0, row1, row2, row3, row4)
+
+    var row0Exp1: Seq[Char] = ".....".toSeq
+    var row1Exp1: Seq[Char] = ".....".toSeq
+    var row2Exp1: Seq[Char] = ".#.##".toSeq
+    var row3Exp1: Seq[Char] = ".....".toSeq
+    var row4Exp1: Seq[Char] = ".....".toSeq
+    var afterExplodeOnceFrom12 = Seq(row0Exp1, row1Exp1, row2Exp1, row3Exp1, row4Exp1)
   }
 
   class SetupPuzzleData(name: String) {
@@ -79,30 +86,74 @@ class Puzzle1Test extends AnyFlatSpec {
     assert(numberVisible(m, Pos(4, 3)) == 7)
     assert(numberVisible(m, Pos(4, 4)) == 7)
     assert(numberVisible(m, Pos(3, 4)) == 8)
-    assert(mostVisible(m) == 8)
+    assert(mostVisible(m)._1 == 8)
   }
 
-    it should "handle puzzle example 1" in new SetupPuzzleData("example1") {
-      assert(numberVisible(map, Pos(5, 8)) == 33)
-      assert(mostVisible(map) == 33)
-    }
+  it should "handle puzzle example 1" in new SetupPuzzleData("example1") {
+    assert(numberVisible(map, Pos(5, 8)) == 33)
+    assert(mostVisible(map)._1 == 33)
+  }
 
-    it should "handle puzzle example 2" in new SetupPuzzleData("example2") {
-      assert(numberVisible(map, Pos(1, 2)) == 35)
-      assert(mostVisible(map) == 35)
-    }
+  it should "handle puzzle example 2" in new SetupPuzzleData("example2") {
+    assert(numberVisible(map, Pos(1, 2)) == 35)
+    assert(mostVisible(map)._1 == 35)
+  }
 
-    it should "handle puzzle example 3" in new SetupPuzzleData("example3") {
-      assert(numberVisible(map, Pos(6, 3)) == 41)
-      assert(mostVisible(map) == 41)
-    }
+  it should "handle puzzle example 3" in new SetupPuzzleData("example3") {
+    assert(numberVisible(map, Pos(6, 3)) == 41)
+    assert(mostVisible(map)._1 == 41)
+  }
 
-    it should "handle puzzle example 4" in new SetupPuzzleData("example4") {
-      assert(numberVisible(map, Pos(11, 13)) == 210)
-      assert(mostVisible(map) == 210)
-    }
+  it should "handle puzzle example 4" in new SetupPuzzleData("example4") {
+    assert(numberVisible(map, Pos(11, 13)) == 210)
+    assert(mostVisible(map)._1 == 210)
+  }
 
-    it should "solve the puzzle" in new SetupPuzzleData("input") {
-      assert(mostVisible(map) == 278)
-    }
+  it should "solve the puzzle part 1" in new SetupPuzzleData("input") {
+    assert(mostVisible(map)._1 == 278)
+  }
+
+  behavior of "visibleSortedClockwise"
+  it should "handle small example" in new SetupSmallExampleData {
+    assert(visibleSortedClockwise(m, Pos(1, 2)) ==
+      Seq(Pos(1, 0), Pos(4, 0), Pos(2, 2), Pos(4, 3), Pos(4, 4), Pos(3, 4), Pos(0, 2)))
+    assert(visibleSortedClockwise(m, Pos(4, 0)) ==
+      Seq(Pos(4, 2), Pos(3, 4), Pos(3, 2), Pos(2, 2), Pos(1, 2), Pos(0, 2), Pos(1, 0)))
+    assert(visibleSortedClockwise(m, Pos(4, 4)) ==
+      Seq(Pos(4, 3), Pos(3, 4), Pos(0, 2), Pos(1, 2), Pos(2, 2), Pos(1, 0), Pos(3, 2)))
+    assert(visibleSortedClockwise(m, Pos(0, 2)) ==
+      Seq(Pos(1, 0), Pos(4, 0), Pos(1, 2), Pos(4, 3), Pos(4, 4), Pos(3, 4)))
+  }
+
+  behavior of "explode"
+  it should "handle small example" in new SetupSmallExampleData {
+    assert(explode(m, visibleSortedClockwise(m, Pos(1, 2))) == afterExplodeOnceFrom12)
+  }
+
+  behavior of "explosionOrderFrom"
+  it should "handle small example" in new SetupSmallExampleData {
+    val order = Seq(Pos(1, 0), Pos(4, 0), Pos(2, 2), Pos(4, 3), Pos(4, 4), Pos(3, 4), Pos(0, 2), Pos(3, 2), Pos(4, 2))
+    assert(explosionOrderFrom(m, Pos(1, 2)) == order)
+  }
+
+  it should "handle puzzle example 4" in new SetupPuzzleData("example4") {
+    val order: Seq[Pos] = explosionOrderFrom(map, Pos(11, 13))
+    assert(order.size == 299)
+    assert(order.head == Pos(11, 12))
+    assert(order(1) == Pos(12, 1))
+    assert(order(2) == Pos(12, 2))
+    assert(order(9) == Pos(12, 8))
+    assert(order(19) == Pos(16, 0))
+    assert(order(49) == Pos(16, 9))
+    assert(order(99) == Pos(10, 16))
+    assert(order(198) == Pos(9, 6))
+    assert(order(199) == Pos(8, 2))
+    assert(order(200) == Pos(10, 9))
+    assert(order(298) == Pos(11, 1))
+  }
+
+  it should "solve the puzzle part 2" in new SetupPuzzleData("input") {
+    val order: Seq[Pos] = explosionOrderFrom(map, mostVisible(map)._2)
+    assert(order(199) == Pos(14, 17))
+  }
 }
