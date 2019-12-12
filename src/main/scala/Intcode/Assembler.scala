42939f461a24de601ae64366abbfb3e9c7b93ebd
kelley.glenn <kelley.glenn@workday.com>
2019-12-11 20:20:36 -0800

    Created Intcode Assembler

---
+++ src/main/scala/Intcode/Assembler.scala
@@ -0,0 +1,221 @@
+package Intcode
+
+import scala.util.matching.Regex
+import scala.util.matching.Regex.MatchIterator
+
+object Assembler {
+
+  class ParseException(t: Exception, line: Int) extends Exception("line(" + line + ")", t)
+
+  class Op(val id: Int) {
+    def validate(rawOpLines: RawOpLine): Unit = {}
+  }
+
+  class OpWithOutputParam(id: Int, outputPos: Int) extends Op(id) {
+    override def validate(rawOpLine: RawOpLine): Unit = {
+      if (rawOpLine.params(outputPos).mode == Mode.Immediate)
+        throw new IllegalArgumentException("Parameter " + (outputPos + 1) + " cannot be Immediate mode")
+    }
+  }
+
+  object NOP0 extends Op(0)
+
+  object NOP1 extends Op(0)
+
+  object NOP2 extends Op(0)
+
+  object NOP3 extends Op(0)
+
+  object ADD extends OpWithOutputParam(1, 2)
+
+  object MULT extends OpWithOutputParam(2, 2)
+
+  object IN extends OpWithOutputParam(3, 0)
+
+  object OUT extends Op(4)
+
+  object JNEZ extends Op(5)
+
+  object JEQZ extends Op(6)
+
+  object LESS extends OpWithOutputParam(7, 2)
+
+  object EQ extends OpWithOutputParam(8, 2)
+
+  object INCB extends Op(9)
+
+  object END extends Op(99)
+
+  val opLookup: Map[String, Op] = {
+    val opSeq: Set[Op] = Set(NOP0, NOP1, NOP2, NOP3, ADD, MULT, IN, OUT, JNEZ, JEQZ, LESS, EQ, INCB, END)
+    opSeq.map(op => (op.getClass.getSimpleName.dropRight(1), op)).toMap
+  }
+
+  object Mode extends Enumeration {
+    type Mode = Value
+    val Positional, Immediate, Relative = Value
+  }
+
+  case class RawParam(mode: Mode.Value, value: Either[Long, String])
+
+  case class Param(mode: Mode.Value, value: Long)
+
+  case class RawOpLine(op: Op, params: Seq[RawParam], startPosition: Int, nextPosition: Int)
+
+  case class OpLine(op: Op, params: Seq[Param])
+
+  val commentGroup: String = ";(.*)"
+  val commentAfterGroup: String = raw"(?:\s+;(.*))?"
+  val commentLineReg: Regex = commentGroup.r("comment")
+  val bytePosGroup: String = raw"\s*(\d+)\:\s*"
+  val createLabelGroup: String = raw"(?::(\w+))?"
+  val paramGroup: String = raw"\s*[\s,]\s*((?:(\@)?(-?\d+)|\@(\w+))(\+B)?)" + createLabelGroup
+  val op0lineGroupReg: Regex =
+    (bytePosGroup + "(NOP0|END)" + createLabelGroup + commentAfterGroup)
+      .r("bytePos", "op", "opLabel", "comment")
+  val op1lineGroupReg: Regex =
+    (bytePosGroup + "(NOP1|IN|OUT|INCB)" + createLabelGroup + paramGroup + commentAfterGroup)
+      .r("bytePos", "op", "opLabel",
+        "param", "positional", "number", "referencedLabel", "relative", "createdLabel", "comment")
+  val op2lineGroupReg: Regex =
+    (bytePosGroup + "(NOP2|JNEZ|JEQZ)" + createLabelGroup + paramGroup + paramGroup + commentAfterGroup)
+      .r("bytePos", "op", "opLabel",
+        "param1", "positional1", "number1", "referencedLabel1", "relative1", "createdLabel1",
+        "param2", "positional2", "number2", "referencedLabel2", "relative2", "createdLabel2", "comment")
+  val op3lineGroupReg: Regex =
+    (bytePosGroup + "(NOP3|ADD|MULT|LESS|EQ)" + createLabelGroup + paramGroup + paramGroup + paramGroup + commentAfterGroup)
+      .r("bytePos", "op", "opLabel",
+        "param1", "positional1", "number1", "referencedLabel1", "relative1", "createdLabel1",
+        "param2", "positional2", "number2", "referencedLabel2", "relative2", "createdLabel2",
+        "param3", "positional3", "number3", "referencedLabel3", "relative3", "createdLabel3", "comment")
+
+  var labelMap: Map[String, Int] = Map.empty
+
+  def rawParamWithSuffix(g: MatchIterator, suffix: String): RawParam = {
+    val m: Mode.Value = {
+      val positional: Boolean = g.group("positional" + suffix) != null || g.group("referencedLabel" + suffix) != null
+      val relative: Boolean = g.group("relative" + suffix) != null
+      if (!positional && !relative) Mode.Immediate
+      else if (positional && !relative) Mode.Positional
+      else if (positional && relative) Mode.Relative
+      else throw new IllegalArgumentException("Relative params must also be positional")
+    }
+    val value: Either[Long, String] = {
+      val numericVal: String = g.group("number" + suffix)
+      if (numericVal != null) Left(numericVal.toLong)
+      else Right(g.group("referencedLabel" + suffix))
+    }
+    RawParam(m, value)
+  }
+
+  def addLabel(label: String, position: Int): Unit = {
+    if (label != null) labelMap = labelMap + (label -> position)
+  }
+
+  def parseCommon(g: MatchIterator, position: Int): Unit = {
+    if (g.group("bytePos").toInt != position) throw new IllegalArgumentException("Byte memoryPos should be " + position)
+    addLabel(g.group("opLabel"), position)
+  }
+
+  def addLabels(g: MatchIterator, suffixes: Set[(String, Int)]): Unit = {
+    suffixes.foreach(s => addLabel(g.group("createdLabel" + s._1), s._2))
+  }
+
+  def parseLine(line: String, memoryPos: Int): RawOpLine = {
+    if (op0lineGroupReg.matches(line)) {
+      val g: MatchIterator = op0lineGroupReg.findAllIn(line)
+      parseCommon(g, memoryPos)
+      RawOpLine(opLookup(g.group("op")), Seq.empty, memoryPos, memoryPos + 1)
+    } else if (op1lineGroupReg.matches(line)) {
+      val g: MatchIterator = op1lineGroupReg.findAllIn(line)
+      parseCommon(g, memoryPos)
+      val p: RawParam = rawParamWithSuffix(g, "")
+      addLabels(g, Set(("", memoryPos + 1)))
+      RawOpLine(opLookup(g.group("op")), Seq(p), memoryPos, memoryPos + 2)
+    }
+    else if (op2lineGroupReg.matches(line)) {
+      val g: MatchIterator = op2lineGroupReg.findAllIn(line)
+      parseCommon(g, memoryPos)
+      val p1: RawParam = rawParamWithSuffix(g, "1")
+      val p2: RawParam = rawParamWithSuffix(g, "2")
+      addLabels(g, Set(("1", memoryPos + 1), ("2", memoryPos + 2)))
+      RawOpLine(opLookup(g.group("op")), Seq(p1, p2), memoryPos, memoryPos + 3)
+    }
+    else if (op3lineGroupReg.matches(line)) {
+      val g: MatchIterator = op3lineGroupReg.findAllIn(line)
+      parseCommon(g, memoryPos)
+      val p1: RawParam = rawParamWithSuffix(g, "1")
+      val p2: RawParam = rawParamWithSuffix(g, "2")
+      val p3: RawParam = rawParamWithSuffix(g, "3")
+      addLabels(g, Set(("1", memoryPos + 1), ("2", memoryPos + 2), ("3", memoryPos + 3)))
+      RawOpLine(opLookup(g.group("op")), Seq(p1, p2, p3), memoryPos, memoryPos + 4)
+    }
+    else throw new IllegalArgumentException("Unable to parseLines line: " + line)
+  }
+
+  def parseLines(lines: Seq[String], curMemoryPosition: Int = 0, lineNumber: Int = 0): Seq[RawOpLine] = {
+    if (lines.isEmpty) Seq.empty
+    else if (commentLineReg.matches(lines.head)) parseLines(lines.tail, curMemoryPosition)
+    else {
+      try {
+        val newLine: RawOpLine = parseLine(lines.head, curMemoryPosition)
+        newLine.op.validate(newLine)
+        newLine +: parseLines(lines.tail, newLine.nextPosition, lineNumber + 1)
+      }
+      catch {
+        case iae: IllegalArgumentException => throw new ParseException(iae, lineNumber)
+      }
+    }
+  }
+
+  def cookRawParam(rawParam: RawParam): Param = {
+    Param(rawParam.mode, rawParam.value match {
+      case Left(l) => l
+      case Right(s) => if (labelMap.contains(s)) labelMap(s).toLong
+      else throw new IllegalArgumentException("label '" + s + "' was used but not defined")
+    })
+  }
+
+  def cookRawParams(params: Seq[RawParam]): Seq[Param] = {
+    if (params.isEmpty) Seq.empty
+    else cookRawParam(params.head) +: cookRawParams(params.tail)
+  }
+
+  def cookRawOpLine(line: RawOpLine): OpLine = {
+    OpLine(line.op, cookRawParams(line.params))
+  }
+
+  def cookRawOpLines(rawOpLines: Seq[RawOpLine]): Seq[OpLine] = {
+    if (rawOpLines.isEmpty) Seq.empty
+    else cookRawOpLine(rawOpLines.head) +: cookRawOpLines(rawOpLines.tail)
+  }
+
+  def mode(param: Param): Short = {
+    param.mode.id.toShort
+  }
+
+  def mode(params: Seq[Param]): Short = {
+    if (params.isEmpty) 0
+    else (mode(params.tail) * 10 + mode(params.head)).toShort
+  }
+
+  def serializeParams(params: Seq[Param]): Seq[Long] = {
+    if (params.isEmpty) Seq.empty
+    else params.head.value +: serializeParams(params.tail)
+  }
+
+  def serializeOpLine(opLine: OpLine): Seq[Long] = {
+    (mode(opLine.params) * 100 + opLine.op.id).toLong +: serializeParams(opLine.params)
+  }
+
+  def serializeOpLines(opLines: Seq[OpLine]): Seq[Long] = {
+    if (opLines.isEmpty) Seq.empty
+    else serializeOpLine(opLines.head) ++ serializeOpLines(opLines.tail)
+  }
+
+  def assemble(lines: Seq[String]): Seq[Long] = {
+    val rawOpLines: Seq[RawOpLine] = parseLines(lines)
+    val opLines: Seq[OpLine] = cookRawOpLines(rawOpLines)
+    serializeOpLines(opLines)
+  }
+}
---
+++ src/test/scala/Intcode/AssemblerTest.scala
@@ -0,0 +1,192 @@
+package Intcode
+
+import Intcode.Assembler._
+import org.scalatest.flatspec.AnyFlatSpec
+
+import scala.util.matching.Regex
+
+class AssemblerTest extends AnyFlatSpec {
+  behavior of "Regex groups"
+  it must "define bytePosGroup correctly" in {
+    val regEx: Regex = bytePosGroup.r
+    assert(regEx.matches("123:"))
+    assert(regEx.matches(" 123:"))
+    assert(regEx.matches(" 123:  "))
+    assert(regEx.matches("\t\t123:\t"))
+    assert(!regEx.matches("ABC"))
+  }
+
+  it must "define op0lineGroupReg correctly" in {
+    assert(op0lineGroupReg.matches(" 1: NOP0"))
+    assert(op0lineGroupReg.matches(" 1: NOP0:start ;start here"))
+    assert(op0lineGroupReg.findAllIn("12: END").group("bytePos") == "12")
+    assert(op0lineGroupReg.findAllIn("12: END").group("op") == "END")
+    assert(op0lineGroupReg.findAllIn(" 1: NOP0:start ;start here").group("opLabel") == "start")
+    assert(op0lineGroupReg.findAllIn(" 1: NOP0:start ;start here").group("comment") == "start here")
+  }
+
+  it must "define paramGroup correctly" in {
+    val regEx: Regex = paramGroup.r
+    assert(regEx.matches(" 123"))
+    assert(regEx.matches(" @123"))
+    assert(regEx.matches(",@123+B:here"))
+    assert(regEx.matches(",@there:here"))
+    assert(!regEx.matches("there:here"))
+  }
+
+  it must "define op1lineGroupReg correctly" in {
+    val regEx: Regex = op1lineGroupReg
+    assert(regEx.matches(" 1: NOP1 123"))
+    assert(regEx.matches(" 1: IN 123"))
+    assert(regEx.matches(" 1: OUT 123"))
+    assert(regEx.matches(" 1: INCB 123"))
+    assert(regEx.matches(" 1: INCB -123"))
+    assert(!regEx.matches(" 1: IN"))
+    val line1 = " 1: NOP1:start @123+B ;start here"
+    assert(regEx.matches(line1))
+    assert(regEx.findAllIn(line1).group("bytePos") == "1")
+    assert(regEx.findAllIn(line1).group("op") == "NOP1")
+    assert(regEx.findAllIn(line1).group("opLabel") == "start")
+    assert(regEx.findAllIn(line1).group("param") == "@123+B")
+    assert(regEx.findAllIn(line1).group("positional") == "@")
+    assert(regEx.findAllIn(line1).group("number") == "123")
+    assert(regEx.findAllIn(line1).group("referencedLabel") == null)
+    assert(regEx.findAllIn(line1).group("relative") == "+B")
+    assert(regEx.findAllIn(line1).group("createdLabel") == null)
+    assert(regEx.findAllIn(line1).group("comment") == "start here")
+    val line2 = " 2: IN,@end+B:some ;end here"
+    assert(regEx.matches(line2))
+    assert(regEx.findAllIn(line2).group("bytePos") == "2")
+    assert(regEx.findAllIn(line2).group("op") == "IN")
+    assert(regEx.findAllIn(line2).group("opLabel") == null)
+    assert(regEx.findAllIn(line2).group("param") == "@end+B")
+    assert(regEx.findAllIn(line2).group("positional") == null)
+    assert(regEx.findAllIn(line2).group("number") == null)
+    assert(regEx.findAllIn(line2).group("referencedLabel") == "end")
+    assert(regEx.findAllIn(line2).group("relative") == "+B")
+    assert(regEx.findAllIn(line2).group("createdLabel") == "some")
+    assert(regEx.findAllIn(line2).group("comment") == "end here")
+  }
+
+  it must "define op2lineGroupReg correctly" in {
+    val regEx: Regex = op2lineGroupReg
+    assert(regEx.matches(" 1: NOP2 123 @321"))
+    assert(regEx.matches(" 1: JNEZ 123 @321"))
+    assert(regEx.matches(" 1: JEQZ 123 @321"))
+    assert(!regEx.matches(" 1: JNEZ 123"))
+    val line1 = " 1: JNEZ:start @123+B @tgt:src ;start here"
+    assert(regEx.matches(line1))
+    assert(regEx.findAllIn(line1).group("bytePos") == "1")
+    assert(regEx.findAllIn(line1).group("op") == "JNEZ")
+    assert(regEx.findAllIn(line1).group("opLabel") == "start")
+    assert(regEx.findAllIn(line1).group("param1") == "@123+B")
+    assert(regEx.findAllIn(line1).group("positional1") == "@")
+    assert(regEx.findAllIn(line1).group("number1") == "123")
+    assert(regEx.findAllIn(line1).group("referencedLabel1") == null)
+    assert(regEx.findAllIn(line1).group("relative1") == "+B")
+    assert(regEx.findAllIn(line1).group("createdLabel1") == null)
+    assert(regEx.findAllIn(line1).group("param2") == "@tgt")
+    assert(regEx.findAllIn(line1).group("positional2") == null)
+    assert(regEx.findAllIn(line1).group("number2") == null)
+    assert(regEx.findAllIn(line1).group("referencedLabel2") == "tgt")
+    assert(regEx.findAllIn(line1).group("relative2") == null)
+    assert(regEx.findAllIn(line1).group("createdLabel2") == "src")
+    assert(regEx.findAllIn(line1).group("comment") == "start here")
+  }
+
+  it must "define op3lineGroupReg correctly" in {
+    val regEx: Regex = op3lineGroupReg
+    assert(regEx.matches(" 1: NOP3 123 @321 @tgt"))
+    assert(regEx.matches(" 1: ADD 123 @321 @tgt"))
+    assert(regEx.matches(" 1: MULT 123 @321 @tgt"))
+    assert(regEx.matches(" 1: LESS 123 @321 @tgt"))
+    assert(regEx.matches(" 1: EQ 123 @321 @tgt"))
+    assert(!regEx.matches(" 1: ADD 123 @321"))
+    val line1 = " 1: ADD:start @123+B 321 @tgt:src ;start here"
+    assert(regEx.findAllIn(line1).group("bytePos") == "1")
+    assert(regEx.findAllIn(line1).group("op") == "ADD")
+    assert(regEx.findAllIn(line1).group("opLabel") == "start")
+    assert(regEx.findAllIn(line1).group("param1") == "@123+B")
+    assert(regEx.findAllIn(line1).group("positional1") == "@")
+    assert(regEx.findAllIn(line1).group("number1") == "123")
+    assert(regEx.findAllIn(line1).group("referencedLabel1") == null)
+    assert(regEx.findAllIn(line1).group("relative1") == "+B")
+    assert(regEx.findAllIn(line1).group("createdLabel1") == null)
+    assert(regEx.findAllIn(line1).group("param2") == "321")
+    assert(regEx.findAllIn(line1).group("positional2") == null)
+    assert(regEx.findAllIn(line1).group("number2") == "321")
+    assert(regEx.findAllIn(line1).group("referencedLabel2") == null)
+    assert(regEx.findAllIn(line1).group("relative2") == null)
+    assert(regEx.findAllIn(line1).group("createdLabel2") == null)
+    assert(regEx.findAllIn(line1).group("param3") == "@tgt")
+    assert(regEx.findAllIn(line1).group("positional3") == null)
+    assert(regEx.findAllIn(line1).group("number3") == null)
+    assert(regEx.findAllIn(line1).group("referencedLabel3") == "tgt")
+    assert(regEx.findAllIn(line1).group("relative3") == null)
+    assert(regEx.findAllIn(line1).group("createdLabel3") == "src")
+    assert(regEx.findAllIn(line1).group("comment") == "start here")
+  }
+
+  behavior of "opLookup"
+  it should "handle all possible values" in {
+    println(opLookup)
+    assert(opLookup.contains("NOP0"))
+    assert(opLookup("NOP0") == NOP0)
+    assert(opLookup("NOP0").id == 0)
+    assert(opLookup("NOP1") == NOP1)
+    assert(opLookup("NOP1").id == 0)
+    assert(opLookup("ADD") == ADD)
+    assert(opLookup("ADD").id == 1)
+    assert(opLookup("END") == END)
+    assert(opLookup("END").id == 99)
+  }
+
+  behavior of "assemble"
+  it should "handle single lines" in {
+    assert(Assembler.assemble(Seq(" 0: NOP0")) == Seq(0))
+    assert(Assembler.assemble(Seq(" 0: NOP1 @1")) == Seq(0, 1))
+    assert(Assembler.assemble(Seq(" 0: NOP2 1 2")) == Seq(1100, 1, 2))
+    assert(Assembler.assemble(Seq(" 0: NOP3 1 2 3")) == Seq(11100, 1, 2, 3))
+    assert(Assembler.assemble(Seq(" 0: ADD 0 0 @0")) == Seq(1101, 0, 0, 0))
+    assert(Assembler.assemble(Seq(" 0: MULT @1 @2 @3")) == Seq(2, 1, 2, 3))
+    assert(Assembler.assemble(Seq(" 0: IN @1+B")) == Seq(203, 1))
+    assert(Assembler.assemble(Seq(" 0: OUT 1")) == Seq(104, 1))
+    assert(Assembler.assemble(Seq(" 0: JNEZ 1 2")) == Seq(1105, 1, 2))
+    assert(Assembler.assemble(Seq(" 0: JEQZ @1 @2")) == Seq(6, 1, 2))
+    assert(Assembler.assemble(Seq(" 0: LESS @1+B,@2+B,@3+B")) == Seq(22207, 1, 2, 3))
+    assert(Assembler.assemble(Seq(" 0: EQ 1, 2, @3")) == Seq(1108, 1, 2, 3))
+    assert(Assembler.assemble(Seq(" 0: INCB @1")) == Seq(9, 1))
+    assert(Assembler.assemble(Seq(" 0: END")) == Seq(99))
+  }
+
+  it should "handle labels" in {
+    assert(Assembler.assemble(Seq(" 0: NOP1:start @start")) == Seq(0, 0))
+    assert(Assembler.assemble(Seq(" 0: ADD 1, 5, @2")) == Seq(1101, 1, 5, 2))
+    assert(Assembler.assemble(Seq(" 0: ADD 1, 5:dest, @dest")) == Seq(1101, 1, 5, 2))
+    assert(Assembler.assemble(Seq(" 0: ADD @dest, 5:dest, @10")) == Seq(1001, 2, 5, 10))
+  }
+
+  it should "disallow +B without @" in {
+    assertThrows[ParseException](Assembler.assemble(Seq(" 0: IN 1+B")))
+  }
+
+  it should "disallow immediates in write-only params" in {
+    assertThrows[ParseException](Assembler.assemble(Seq(" 0: ADD 0 0 0")))
+    assertThrows[ParseException](Assembler.assemble(Seq(" 0: MULT @1 @2 3")))
+    assertThrows[ParseException](Assembler.assemble(Seq(" 0: IN 1")))
+    assertThrows[ParseException](Assembler.assemble(Seq(" 0: LESS @1 @2 3")))
+    assertThrows[ParseException](Assembler.assemble(Seq(" 0: EQ @1 @2 3")))
+  }
+
+  it should "handle multiple lines" in {
+    val lines:Seq[String] = Seq(
+      " 0: INCB 1",
+      " 2: OUT @-1+B",
+      " 4: ADD @100 1 @100",
+      " 8: EQ @100 16 @101",
+      "12: JEQZ @101, 0",
+      "15: END"
+    )
+    assert(Assembler.assemble(lines) == Seq(109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99))
+  }
+}
